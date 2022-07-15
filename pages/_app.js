2241f1c4e69eff2da4916796ac3127568a18b36b
ikealmighty <isaacyates7@gmail.com>
2022-07-15 16:46:58 -0500

    added react toastify

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import { Auth0Provider } from "@auth0/auth0-react";
+import { ToastContainer } from "react-toastify";
 import "../public/globals.css";
+import "react-toastify/dist/ReactToastify.min.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
@@ -8,6 +10,7 @@ export default function MyApp({ Component, pageProps }) {
       domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
       redirectUri={process.env.NEXT_PUBLIC_AUTH0_REDIRECT}
     >
+      <ToastContainer />
       <Component {...pageProps} />
     </Auth0Provider>
   );
